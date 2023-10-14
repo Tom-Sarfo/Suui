268675c1a6fd954bed1632f8b05cd45c8c5b856f
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-10-14 03:51:08 -0700

    Add a function that prints characters to the screen

---
+++ simple_shell.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+/**
+ * main - entry point of simple shell
+ * Return: 0
+ * Description: This is the entry point of a simple shell project
+ */
+
+int main(void)
+{
+
+	while (true)
+	{
+		kyere_prompt();
+		gye_input();
+	}
+	return (0);
+}
