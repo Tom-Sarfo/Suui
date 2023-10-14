24e266ba481e4c1eb8b8eb78336ab31cabf946e4
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-10-14 06:36:48 -0700

    Add update to str_str function

--- str_str.c
+++ str_str.c
@@ -1,30 +1,32 @@
 #include "shell.h"
 
 /**
- * _strstr - a function that locates a substring
+ * str_str - a function that locates a substring
  * @needle: the substring ti be found
  * @haystack: the string searched
  * Return: pointer to substring address(success) or NULL (failure)
  */
 
-char *_strstr(char *haystack, char *needle)
+char *str_str(char *haystack, char *needle)
 {
-        int x;
+	int x;
 
-        if (*needle == 0)
-                return (haystack);
-        while (*haystack)
-        {
-                x = 0;
-        if (haystack[x] == needle[x])
-        {
-                do {
-                        if (needle[x + 1] == '\0')
-                                return (haystack);
-                        x++;
-        } while (haystack[x] == needle[x]);
-        }
-                haystack++;
-        }
-        return ('\0');
+	if (*needle == 0)
+		return (haystack);
+	while (*haystack)
+	{
+		x = 0;
+		if (haystack[x] == needle[x])
+		{
+			do 
+			{
+				if (needle[x + 1] == '\0')
+					return (haystack);
+				x++;
+			}
+			while (haystack[x] == needle[x]);
+		}
+		haystack++;
+	}
+	return ('\0');
 }
