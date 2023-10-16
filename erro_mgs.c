f1069d5fca1684ecf9e25871856022c4daccb992
MphoMpepeto <mpho.mpepeto@alumni.uct.ac.za>
2023-10-16 07:29:38 -0700

    erro msg

---
+++ erro_mgs.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+
+/**
+ *
+ */
+
+char *erro_mgs(int counter)
+{
+	char after_pipe;
+	char cwd[PATH_MAX];
+	const char *end_mgs;
+	char *erro;
+
+	after_pipe = getcwd(cwd, sizeof(cwd));
+
+
