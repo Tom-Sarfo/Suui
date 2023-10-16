38c25e12f8d8d0dfcd0ecd9b7f521308099462b8
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-10-16 08:24:18 -0700

    test

--- erro_mgs.c
+++ erro_mgs.c
@@ -4,13 +4,32 @@
  *
  */
 
-char *erro_mgs(int counter)
+char *erro_mgs(int counter, argv)
 {
 	char after_pipe;
 	char cwd[PATH_MAX];
 	const char *end_mgs;
 	char *erro;
+	char *colon = ": ";
+	char error_msg[10];;
 
 	after_pipe = getcwd(cwd, sizeof(cwd));
 
+	// have 1. path 2. counter 3. argv, error message
+//	./hsh: 1: qwerty : not found
+	write(2, after_pipe, strlen(after_pipe));
+	write(2, colon, strlen(colon));
+	write(2, counter, strlen(counter));
+	write(2, colon, strlen(colon));
+	write(2, argv, strlen(argv));
+	write(2, colon, strlen(colon));
+	write(2, error_msg, strlen(error_msg);
+        strings[];
 
+	string[0] = after_pipe;
+	string[1] = colon;
+	------- = -----;
+
+	error = write(2, string[], strlen(string[]));
+
+	return (error)
