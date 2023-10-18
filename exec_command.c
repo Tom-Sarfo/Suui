3eba4df91d7eab3ae7e1b45d64d8520bc2b0ca8f
Tom-Sarfo <tomsarfodavis@gmail.com>
2023-10-18 01:52:54 -0700

    try

--- exec_command.c
+++ exec_command.c
@@ -7,7 +7,7 @@
  */
 void yenza_command(char *nipa_input)
 {
-	pid_t cp_id = fork();
+	/* pid_t cp_id = fork(); */
 	int pokemon_count = 0;
 	char *pokemon, *delimetr = " \n", **array_of_pokemons;
 	char *copy_nipa = malloc(sizeof(char) * strlen(nipa_input));
@@ -22,13 +22,13 @@ void yenza_command(char *nipa_input)
 		pokemon_count++;
 		pokemon = strtok(NULL, delimetr);
 	}
-	if (cp_id == -1)
+	/* if (cp_id == -1)
 	{
 		perror("fork");
 		exit(EXIT_FAILURE);
-	}
+	}*/
 	check_malloc(array_of_pokemons);
-	if (cp_id == 0)
+	/* if (cp_id == 0)
 	{
 		ye_commd(copy_nipa, array_of_pokemons, pokemon, delimetr);
 	}
